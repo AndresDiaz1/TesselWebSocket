285e0fcff5abc0444606b031493e0bf60106f3e9
AndresDiaz1 <andiazvar@gmail.com>
2016-08-07 18:03:30 -0500

    Funciona obtener temperatura periodicamente y encender led on click

--- Unity3D/UnityAndTessel/Assets/DZBZ_ClickGameObject/ClickGameObjectHandler.cs
+++ Unity3D/UnityAndTessel/Assets/DZBZ_ClickGameObject/ClickGameObjectHandler.cs
@@ -20,7 +20,7 @@ public class ClickGameObjectHandler : MonoBehaviour {
         if (rb)
         {
             rb.AddForce(transform.up * 5F, ForceMode.Impulse);
-			//StartCoroutine(GetLED());
+			StartCoroutine(GetLED());
         }
         else Debug.LogError("no rigid body on clicked object name=" + obj.name);
     }
@@ -43,6 +43,23 @@ public class ClickGameObjectHandler : MonoBehaviour {
 		}
 	}
 
+	IEnumerator GetLED()
+	{
+		string url = "http://192.168.0.7:8080/leds/0";
+		UnityWebRequest www = UnityWebRequest.Get (url);
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log(www.error);
+		}
+		else {
+			// Show results as text
+			Debug.Log(www.downloadHandler.text);
+
+			// Or retrieve results as binary data
+			byte[] results = www.downloadHandler.data;
+		}
+	}
 
 
 	void Example() {
